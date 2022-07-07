f9b9389a107d0cf02d224bff58afab8e14b2ab1b
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-07 18:33:37 -0400

    index file

---
+++ index.js
@@ -0,0 +1,12 @@
+const {fetchBreedDescription} = require('./breedFetcher');
+
+const breedName = process.argv[2];
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error: ', error);
+  } else {
+    console.log(desc);
+  }
+});
+
+
